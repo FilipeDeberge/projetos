17529492f9e969973c77bd1c39e1d4c9282ec7f2
FilipeDeberge <filipedeberge@gmail.com>
2022-07-19 10:04:15 -0300

    feat:removendo senha

--- controleGastos/controle_gastos/settings.py
+++ controleGastos/controle_gastos/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'controle_gastos',
         'USER': 'root',
-        'PASSWORD': 'Mefazviver77!',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '3306',
     }
